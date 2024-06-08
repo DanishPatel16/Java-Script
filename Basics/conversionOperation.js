c80a49f228852c56115a4798e4582bcb1a93470f
Danish Patel <86683026+DanishPatel16@users.noreply.github.com>
2024-06-08 20:30:50 +0000

    dataOperation

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -31,6 +31,39 @@ let stringNumner = String (someNumber);
 // console.log(stringNumner);
 // console.log(typeof stringNumner);
 
+//**************************Operations**************************
 
+let value = 10;
+let negValue= -value;
 
+///console.log(negValue);
+
+// console.log(5+5);
+// console.log(5-5);
+// console.log(5/5);
+// console.log(5*5);
+// console.log(5**2);
+
+// console.log('1' + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log('1' + 1 + 1);
+// console.log(1 +1 +'2');
+// console.log( 3 + 4 * 5 % 3);
+// console.log( ((3 + 4 )* 5 )% 3);
+// console.log(35 % 3);
+
+// console.log(+true);
+// // console.log(true+);
+// console.log(+false);
+
+// let a, b, c
+//  a = b=c= 1+1;
+//  console.log(b);
+
+let a = 100
+let b = ++a;
+console.log(b);
+b =a++;
+console.log(b);
 
