1ee5f4a8d35232967a1f5f8d454e41e7f27778a9
Danish Patel <86683026+DanishPatel16@users.noreply.github.com>
2024-06-08 19:58:02 +0000

    conversion of datatypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "undefined";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"danish" => NaN
+//Undefined => NaN
+// true => 1, Fales => 0;
+//null =>0;
+
+
+let isLoggesIn = "danish"
+let booleanIsLoggesIn = Boolean (isLoggesIn);
+
+// console.log(booleanIsLoggesIn);
+
+// 1 => truem ,0 => false;
+// '' => false;
+//"danish" => true;
+
+
+let someNumber = 10
+// console.log(typeof someNumber);
+
+let stringNumner = String (someNumber);
+// console.log(stringNumner);
+// console.log(typeof stringNumner);
+
+
+
+
