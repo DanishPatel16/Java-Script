52b7d1b5828155bcf773404c026da13cfb8cc2de
Danish Patel <86683026+DanishPatel16@users.noreply.github.com>
2024-06-08 19:29:53 +0000

    variables and datatypes

---
+++ Basics/Variables.js
@@ -0,0 +1,24 @@
+const accountID= 1234;
+let accountEmail ="danish.pate@gmail.com";
+var accountPassword ="123";
+accountCity ="Nashik";
+let accountState ;
+console.log(accountID);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+// accountID=1; not allowed
+
+accountEmail ="dp@gmail.com";
+accountPassword="321";
+accountCity ="Pune";
+
+
+// console.log(accountID);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountID, accountEmail , accountPassword, accountCity, accountState]);
+
+// console.log(accountID, accountEmail,   accountPassword, accountCity)
---
+++ Basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all js code as newer version
+
+//alert( 2 + 4) cant use this as this NODE not a browser
+
+// console.log(3+3); 
+// console.log("accountEmail");
+
+let name="Dansih"
+let age=20
+let isLoggesIn = false
+
+// number=> 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null=> standalone value
+//undefined =>
+//symbol => unique
+// Object
+
+console.log(typeof undefined);//undefined 
+console.log(typeof null); // null is nothing but an object
